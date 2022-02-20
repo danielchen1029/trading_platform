5aff0f745e81730504bd2d0c622f9e1981cf05b1
Daniel <Daniel.tpi@chailease.com.tw>
2022-02-20 17:50:51 +0800

    home page done

--- src/app/features/home/pages/index/index.component.ts
+++ src/app/features/home/pages/index/index.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, OnDestroy {
   roadMap: { title: string; items: string[] }[] = [
     {
       title: '2021 Q4',
@@ -53,8 +61,49 @@ export class IndexComponent implements OnInit {
       ],
     },
   ];
+  roadMapResponsive = [
+    {
+      breakpoint: '1199px',
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '559px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
 
+  @ViewChild('nav') navEleRef?: ElementRef<Element>;
+
+  @HostListener('window:scroll')
+  watchScroll() {
+    this.scroll$.next(window.scrollY);
+  }
+
+  private scroll$ = new Subject<number>();
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.scroll$
+      .pipe(debounceTime(100))
+      .subscribe((y) => this.dealWithScroll(y));
+  }
+
+  ngOnDestroy(): void {
+    this.scroll$.complete();
+  }
+
+  private dealWithScroll(scrollY: number): void {
+    if (scrollY >= 900) {
+      this.navEleRef?.nativeElement.setAttribute('style', 'display: block');
+    } else {
+      this.navEleRef?.nativeElement.setAttribute('style', 'display: none');
+    }
+  }
 }
